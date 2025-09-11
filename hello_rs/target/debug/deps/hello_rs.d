270ee4434526ec296108a37016586910e61cbfaa
loveRyujin <1725350830@qq.com>
2025-09-11 23:24:21 +0800

    first commit

---
+++ hello_rs/target/debug/deps/hello_rs.d
@@ -0,0 +1,5 @@
+C:\Users\lichen\code\rust\the_rust_book_demo\hello_rs\target\debug\deps\hello_rs.d: src\main.rs
+
+C:\Users\lichen\code\rust\the_rust_book_demo\hello_rs\target\debug\deps\hello_rs.exe: src\main.rs
+
+src\main.rs:
---
+++ hello_rs/target/debug/hello_rs.d
@@ -0,0 +1,1 @@
+C:\Users\lichen\code\rust\the_rust_book_demo\hello_rs\target\debug\hello_rs.exe: C:\Users\lichen\code\rust\the_rust_book_demo\hello_rs\src\main.rs
