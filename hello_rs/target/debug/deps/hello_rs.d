8165af42332b7c9bc175e86f128c333936c8bc84
loveRyujin <1725350830@qq.com>
2025-09-12 22:16:31 +0800

    remove unused file

--- hello_rs/target/debug/deps/hello_rs.d
+++
@@ -1,5 +0,0 @@
-C:\Users\lichen\code\rust\the_rust_book_demo\hello_rs\target\debug\deps\hello_rs.d: src\main.rs
-
-C:\Users\lichen\code\rust\the_rust_book_demo\hello_rs\target\debug\deps\hello_rs.exe: src\main.rs
-
-src\main.rs:
