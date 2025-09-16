3fc79d00df17c29a0768fc01d43635ea32cb1710
loveRyujin <1725350830@qq.com>
2025-09-16 23:27:41 +0800

    feat: Expose hosting and serving modules publicly
    - Make hosting and serving modules public with public functions
    - Add a new public function demonstrating absolute and relative path usage
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,15 +1,23 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
-        fn seat_at_table() {}
+        pub fn seat_at_table() {}
     }
 
-    mod serving {
-        fn take_order() {}
+    pub mod serving {
+        pub fn take_order() {}
+        
+        pub fn serve_order() {}
 
-        fn serve_order() {}
-
-        fn take_payment() {}
+        pub fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    // 绝对路径
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // 相对路径
+    front_of_house::hosting::add_to_waitlist();
 }
