9122004f2820f4a1ef7f3b459d2b153c6cbb5925
loveRyujin <1725350830@qq.com>
2025-09-26 21:12:40 +0800

    refactor: Improve error handling with Result type
    - Add error handling using the `Error` trait
    - Extract main logic into a separate `run` function that returns a `Result`
    - Replace direct file reading with error propagation using the `?` operator
    - Improve error reporting by printing custom messages instead of panicking
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,10 +14,18 @@ fn main() {
     println!("Searching for {}", cfg.query);
     println!("In file {}", cfg.file_path);
 
-    let contents = fs::read_to_string(cfg.file_path)
-        .expect("Should have been able to read the file");
+    if let Err(e) = run(cfg) {
+        println!("Application run error: {e}!");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
