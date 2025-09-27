ad93e72cb45be78d1989006fdc9dfa4b6c7ba6dd
loveRyujin <1725350830@qq.com>
2025-09-27 12:16:50 +0800

    refactor: Redirect error messages to stderr
    - Change error messages from standard output to standard error
    - Remove debug output printing search configuration
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,15 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let cfg = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problems parsing arguments: {err}!");
+        eprintln!("Problems parsing arguments: {err}!");
         process::exit(1);
     });
 
-    println!("Searching for {}", cfg.query);
-    println!("In file {}", cfg.file_path);
-
     if let Err(e) = minigrep::run(cfg) {
-        println!("Application run error: {e}!");
+        eprintln!("Application run error: {e}!");
         process::exit(1);
     }
 }
