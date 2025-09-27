5865ced347b4b5409ee80af5371537d59e8fd6fb
loveRyujin <1725350830@qq.com>
2025-09-27 11:40:50 +0800

    feat: Add case-insensitive search
    - Add case-insensitive search function that converts both query and content to lowercase
    - Add test for case-insensitive search functionality
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -43,6 +43,19 @@ pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    let query = query.to_lowercase();
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -56,5 +69,20 @@ safe, fast, productive.
 Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
