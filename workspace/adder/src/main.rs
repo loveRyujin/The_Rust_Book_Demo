114b99860621d282f1f712dfbdd9f5643c3169ad
loveRyujin <1725350830@qq.com>
2025-09-28 21:40:39 +0800

    feat: Add add_two crate as a dependency
    - Add new workspace member "add_two"
    - Create new library crate "add_two" with function to add 2 to input
    - Add dependency on "add_two" crate to adder project
    - Include add_two function call in main program output
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

--- workspace/adder/src/main.rs
+++ workspace/adder/src/main.rs
@@ -1,6 +1,8 @@
 use add_one;
+use add_two;
 
 fn main() {
     let num = 10;
     println!("the result of add_one is {}", add_one::add_one(num));
+    println!("the result of add_two is {}", add_two::add_two(num));
 }
