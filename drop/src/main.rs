3666f5212ec1fc3441a88d94f0c2e643cbc6cb61
loveRyujin <1725350830@qq.com>
2025-10-16 22:04:09 +0800

    refactor: Clarify resource cleanup in main function
    - Explicitly drop `s1` before the end of main function
    - Add print statement indicating program execution continues after drop
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

--- drop/src/main.rs
+++ drop/src/main.rs
@@ -10,4 +10,6 @@ fn main() {
     let s1 = CustomStruct(String::from("s1"));
     let s2 = CustomStruct(String::from("s2"));
     println!("s1 and s2 created successfully!");
+    drop(s1);
+    println!("before the end of main function.")
 }
