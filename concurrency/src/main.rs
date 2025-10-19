e89037180bee13ef8d8e31c69da14f179cc0c67f
loveRyujin <1725350830@qq.com>
2025-10-19 18:02:42 +0800

    feat: Add examples for thread creation and ownership
    - Add new function demonstrating basic thread spawning and joining
    - Add new function showing how to move variables into threads with `move` keyword
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -2,6 +2,11 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    thread_process();
+    thread_process_with_move();
+}
+
+fn thread_process() {
     let handler = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} from the spawned thread!");
@@ -13,6 +18,16 @@ fn main() {
         println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
+
+    handler.join().unwrap();
+}
+
+fn thread_process_with_move() {
+    let v = vec![1, 2, 3];
+
+    let handler = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
 
     handler.join().unwrap();
 }
