97d7be5ae9b07df9372238773c9119674f0b8cf8
loveRyujin <1725350830@qq.com>
2025-10-21 21:23:31 +0800

    feat: Implement split_as_mut function for mutable slices
    - Add import for std::slice module
    - Implement split_as_mut function to divide mutable slice into two parts at specified index
    - Add test code demonstrating the split_as_mut function with a vector
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

--- Unsafe/src/main.rs
+++ Unsafe/src/main.rs
@@ -1,3 +1,5 @@
+use std::slice;
+
 fn main() {
     let mut num = 5;
     let r1 =&raw const num;
@@ -11,6 +13,25 @@ fn main() {
     unsafe {
         noop();
     }
+
+    let mut v = vec![1, 2, 3, 4];
+    let (v1, v2) = split_as_mut(&mut v, 2);
+    println!("v1: {:?}", v1);
+    println!("v2: {:?}", v2);
 }
 
 unsafe fn noop() {}
+
+fn split_as_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid)
+        )
+    }
+}
