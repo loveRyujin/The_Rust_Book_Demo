970d02709fabb2b120ffcc800ab8aa9a4dbb0e87
loveRyujin <1725350830@qq.com>
2025-10-22 22:18:45 +0800

    feat: Implement unit conversion for length measurements
    - Add new structs `Millimeters` and `Meters` with derived traits
    - Implement `Add<Meters>` for `Millimeters` to convert meters to millimeters in addition
    - Add test case for adding millimeters and meters
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

--- advanced_trait/src/main.rs
+++ advanced_trait/src/main.rs
@@ -17,10 +17,30 @@ impl Add for Point {
     }
 }
 
+#[derive(Debug, PartialEq)]
+struct Millmeters(i32);
+
+#[derive(Debug, PartialEq)]
+struct Meters(i32);
+
+impl Add<Meters> for Millmeters {
+    type Output = Millmeters;
+
+    fn add(self, other: Meters) -> Millmeters {
+        Millmeters(self.0 + other.0 * 1000)
+    }
+}
+
 fn main() {
     assert_eq!(
         Point {x: 1, y: 1} + Point {x: 2, y: 2},
         Point {x: 3, y: 3}
     );
+
+    assert_eq!(
+        Millmeters(1) + Meters(1),
+        Millmeters(1001)
+    );
+
     println!("successfully assert!")
 }
