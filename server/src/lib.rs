235a4526bb54e168f8790fe989d09ef9ca67704a
loveRyujin <1725350830@qq.com>
2025-10-23 21:42:57 +0800

    refactor: Refactor connection handling to use thread pool
    - Add ThreadPool struct with basic constructor and execute method
    - Create thread pool in main function to handle incoming connections
    - Replace direct thread spawning with thread pool execution for connection handling
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

---
+++ server/src/lib.rs
@@ -0,0 +1,12 @@
+pub struct ThreadPool;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> Self {
+        ThreadPool
+    }
+
+    pub fn execute<F>(&self, f: F) 
+    where F: FnOnce() + Send + 'static {
+        f();
+    }
+}
