b8b5fc973fe249e639a01252c9d2451ec15123b7
loveRyujin <1725350830@qq.com>
2025-10-23 22:14:23 +0800

    feat: Implement a multi-threaded job queue system
    - Implement a multi-threaded thread pool using channels and mutexes
    - Add Worker struct to manage individual threads
    - Replace direct function execution with job queue system
    - Workers continuously listen for and execute jobs from the queue
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,12 +1,54 @@
-pub struct ThreadPool;
+use std::{sync::{mpsc, Arc, Mutex}, thread};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    sender: mpsc::Sender<Job>,
+    workers: Vec<Worker>
+}
 
 impl ThreadPool {
     pub fn new(size: usize) -> Self {
-        ThreadPool
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { sender: sender, workers: workers }
     }
 
     pub fn execute<F>(&self, f: F) 
     where F: FnOnce() + Send + 'static {
-        f();
+        let job = Box::new(f);
+        
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move ||{
+            loop { 
+                let job = receiver.lock().unwrap().recv().unwrap();
+    
+                println!("Worker {id} got a job; executing.");
+    
+                job();
+            }
+        });
+
+        Worker { id: id, thread: thread }
     }
 }
