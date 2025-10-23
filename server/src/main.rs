be4fc892da8413c75d19aad8f332eb41a0168c9b
loveRyujin <1725350830@qq.com>
2025-10-23 20:59:51 +0800

    feat: Serve static files via HTTP server
    - Add HTML file for server response
    - Implement HTTP response with file content serving
    - Remove debug print of HTTP request
    - Reorder import statements
    
    Signed-off-by: loveRyujin <1725350830@qq.com>

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,23 +1,30 @@
-use std::net::{TcpListener, TcpStream};
 use std::io::{BufReader, prelude::*};
+use std::net::{TcpListener, TcpStream};
+use std::fs;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        
+
         handle_connect(stream);
     }
 }
 
-fn handle_connect(stream: TcpStream) {
-    let buf_reader = BufReader::new(stream);
+fn handle_connect(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
 
-    let http_request: Vec<_> = buf_reader
+    let _http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty() )
+        .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {http_request:#?}");
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
